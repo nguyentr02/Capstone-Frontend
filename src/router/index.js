8ec3b11cf226c44f62177084aa3dcd44fcbb7ec2
Hoang Bao Phuc Chau <103523966@student.swin.edu.au>
2024-10-29 16:15:11 +1100

    Added AdminRegistrationManagement and AdminPaymentManagement
    - Added AdminRegistrationManagement.vue and AdminPaymentManagement.vue, which are the content of the pages
    - Added AdminRegistrationManagementView.vue and AdminPaymentManagementView.vue, which are the full pages, contains navbar, sidebar, page content, etc.
    - Added new paths for the pages /adminregistrationmngt and /adminopayment

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,16 @@ import Homepage from '@/views/HomeView.vue'
 import AdminHomeView from "@/views/AdminHomeView.vue";
 import AdminReportView from "@/views/AdminReportView.vue";
 import AdminQuestionaireManagementView from "@/views/AdminQuestionaireManagementView.vue"
+import AdminPaymentManagementView from "@/views/AdminPaymentManagementView.vue"
+import AdminRegistrationManagementView from "@/views/AdminRegistrationManagementView.vue"
 
 const routes = [
   { path: "/", component: Homepage },
   { path: "/admin", component : AdminHomeView},
   { path: "/adminreport", component : AdminReportView},
   { path: "/adminquestionaire", component : AdminQuestionaireManagementView},
+  { path: "/adminpayment", component : AdminPaymentManagementView},
+  { path: "/adminregistrationmngt", component : AdminRegistrationManagementView},
 ];
 
 const router = createRouter({

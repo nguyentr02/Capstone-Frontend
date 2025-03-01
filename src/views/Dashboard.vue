ed3f1162215bdc45f1ea294baceff7d1dc184058
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2025-03-01 17:41:10 +1100

    WIP: styling sidebar for admin dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+    import BaseLayout from '@/components/layout/BaseLayout.vue';
+    import SideBar from '@/components/dashboard/SideBar.vue';
 </script>
 
 <template>
-
+    <div>
+        <SideBar/>
+    </div>
 </template>
 
 <style scoped>

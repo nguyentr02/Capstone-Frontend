881206ad0c6b614b76322a61bfa3749c79335cfb
Minh Phan <113814582+minhphan6624@users.noreply.github.com>
2025-03-01 17:14:03 +1100

    WIP: Designing sidebar

---
+++ src/components/dashboard/SideBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <aside>
+        <div class="aside-head">
+            <span class="image image-cover"><img src="@/assets/cat.jpeg" alt="admin avatar"></span>
+       
+            <div class="admin-name"> John Doe </div>
+            <div class="role"> Administrator </div>
+        </div>
+
+        <div class="aside-body">
+            <ul class = "task-list">\
+                <li class="task-item">
+                    <span class="task-name"> Main</span>
+                </li>
+            </ul>
+        </div>
+    </aside>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/auth/LoginView.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     }
   ],
 })

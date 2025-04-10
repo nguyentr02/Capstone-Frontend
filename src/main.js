2ec12cffd1d68ae4edd561dab49e5ed09fa178a7
nguyentr02 <truongphamtuannguyen@gmail.com>
2025-04-10 19:26:47 +1000

    Install Pinia PersistedState plugin

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 const app = createApp(App)
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate);
 
-app.use(pinia)
+app.use(pinia);
 app.use(router);
 app.mount('#app')
